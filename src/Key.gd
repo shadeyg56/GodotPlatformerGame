8071e3a6af449d2d81b84cfae513a8a710a790a4
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2021-04-04 20:11:40 -0500

    add more keys and locks

--- src/Key.gd
+++ src/Key.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
 signal got_key
-onready var key_obtained = preload("res://assets/HUD/hud_keyYellow.png")
+var key_obtained
+export var color = "yellow"
 
 func _ready():
-	pass # Replace with function body.
+	set_color()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func set_color():
+	var img = load("res://assets/Items/key%s.png" % color.capitalize())
+	$Sprite.texture = img
+	key_obtained = load("res://assets/HUD/hud_key%s.png" % color.capitalize())
+	
 func _on_Key_body_entered(body):
 	if body.name == "Player":
 		body.has_key = true
 		emit_signal("got_key")
-		$"../HUD/MarginContainer/HBoxContainer/Key".texture = key_obtained
+		.get_node("../HUD/MarginContainer/VBoxContainer/HBoxContainer/Keys/%s" % name).texture = key_obtained
 		queue_free()
--- src/Lock.gd
+++ src/Lock.gd
@@ -2,19 +2,26 @@ extends StaticBody2D
 
 onready var area = $Area2D/CollisionShape2D
 onready var collision = $CollisionShape2D
+export var color = "yellow"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_color()
 
-
+func set_color():
+	var img = load("res://assets/Items/lock_%s.png" % color)
+	$Sprite.texture = img
 
 func _on_got_key():
 	area.set_deferred("disabled", false)
 	collision.set_deferred("disabled", true)
+	
 
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		queue_free()
+
+
+func _on_Node_ladder_unlocked():
+	queue_free()
